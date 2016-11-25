459fbd9599a5c9883a47c7bfce97b30c7c2bc2c4
Kolya Keb <kolya31093@ro.ru>
2016-11-25 14:03:05 +0200

    too

--- views/category/view.php
+++ views/category/view.php
@@ -57,7 +57,7 @@ use yii\helpers\Html;
                             <div class="single-products">
                                 <div class="productinfo text-center">
                                     <?= Html::img("@web/images/products/{$product->img}", ['alt' => $product->name])?>
-                                    <h2><?= $product->price ?></h2>
+                                     <h2>$<?= $product->price?></h2>
                                     <p><a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $product->id ])?>"><?= $product->name ?></a></p>
                                     <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                                 </div>
