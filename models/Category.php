d62687144775ad7ef403e0eb9e19beebac453b4c
Kolya Keb <kolya31093@ro.ru>
2016-11-25 14:20:42 +0200

    les10

--- models/Category.php
+++ models/Category.php
@@ -1,23 +1,21 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Andrey
- * Date: 07.05.2016
- * Time: 10:28
+ * User: Kolya
+ * Date: 17.11.2016
+ * Time: 18:14
  */
 
 namespace app\models;
+
 use yii\db\ActiveRecord;
-
-
 class Category extends ActiveRecord{
 
-    public static function tableName(){
-        return 'category';
-    }
+        public static function tableName(){
+            return 'category';
+        }
+         public function getProducts(){
+             return $this->hasMany(Product::className(), ['category_id' => 'id']);
+         }
+}
 
-    public function getProducts(){
-        return $this->hasMany(Product::className(), ['category_id' => 'id']);
-    }
-
-} 
--- models/Product.php
+++ models/Product.php
@@ -1,23 +1,20 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Andrey
- * Date: 07.05.2016
- * Time: 10:28
+ * User: Kolya
+ * Date: 17.11.2016
+ * Time: 18:14
  */
 
 namespace app\models;
+
 use yii\db\ActiveRecord;
-
-
 class Product extends ActiveRecord{
 
     public static function tableName(){
         return 'product';
     }
-
     public function getCategory(){
         return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
-
-} 
+}
